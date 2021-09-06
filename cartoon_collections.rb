c37ee142c081c87ce6486a9ce5325b81fd568ff7
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-09-06 18:22:33 +1000

    Completed Assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.each_with_index{|dwarf, index| puts "#{index+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+#roll_call_dwarves ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def summon_captain_planet summon
+  array = []
+  summon.each{|call| array.push("#{call.capitalize!}!")}
+  array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+#p summon_captain_planet ["earth", "wind", "fire", "water", "heart"]
+
+def long_planeteer_calls array
+  if array.length >= 4
+    return true
+  else
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+#long_planeteer_calls ["two", "go", "industrious"]
+
+def find_the_cheese ingredients
   cheese_types = ["cheddar", "gouda", "camembert"]
+cheese_types.find {|cheese|
+ingredients.include?(cheese)}
 end
+
+snacks = ["crackers", "gouda", "thyme"]
+p find_the_cheese(snacks)
